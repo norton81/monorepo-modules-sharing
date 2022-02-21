ed2a97788b0aa7b3040aba37331a921b80017aa2
Pave Novoseltsev <pnovoseltse@iponweb.net>
2022-02-21 11:57:14 +0300

    recover

--- apps/app-client1/src/app/app.module.ts
+++ apps/app-client1/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule],
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/app-client1/src/decl.d.ts
@@ -0,0 +1,4 @@
+declare module 'appCore/Component3';
+declare module 'appCore/Component2';
+declare module 'appCore/Component1';
+declare module 'appCore/app1Module';
